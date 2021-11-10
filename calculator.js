8c14f6aa114bbbd018ce5a2e92c6470aa9a6b1af
cesardka <cesardka@hotmail.com>
2021-11-09 21:52:50 -0300

    implement add and subtract operations
    Adding the first two basic operations and its tests to dip the foot in
    the water

--- calculator.js
+++ calculator.js
@@ -1,2 +1,19 @@
-module.exports.add = function (numbers) {
+const isValidNumber = (number) => typeof number === "number";
+
+const shouldDoOperation = (firstNumber, secondNumber) =>
+  isValidNumber(firstNumber) && isValidNumber(secondNumber);
+
+module.exports.add = (firstNumber, secondNumber) => {
+  if (!shouldDoOperation(firstNumber, secondNumber)) {
+    return 0;
+  }
+
+  return firstNumber + secondNumber;
+};
+
+module.exports.subtract = (firstNumber, secondNumber) => {
+  if (!shouldDoOperation(firstNumber, secondNumber)) {
+    return 0;
+  }
+  return firstNumber - secondNumber;
 };
